4b7bb7ae094f9c3da512aa7d456575b42958efef
Vladimir Bogomolov <124901578+bva23@users.noreply.github.com>
2023-09-02 17:45:15 +0300

    Add .gitignore

---
+++ Tracker/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  Tracker
+//
+//  Created by Владимир Богомолов on 02.09.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var trackerBlack: UIColor { UIColor(named: "Black") ?? UIColor.black }
+    static var trackerBackground: UIColor { UIColor(named: "Background") ?? UIColor.darkGray }
+    static var trackerRed: UIColor { UIColor(named: "Red") ?? UIColor.red }
+    static var trackerLightGray: UIColor { UIColor(named: "LightGray") ?? UIColor.lightGray }
+    static var trackerGray: UIColor { UIColor(named: "Gray") ?? UIColor.gray }
+    static var trackerWhite: UIColor { UIColor(named: "White") ?? UIColor.white }
+    static var trackerBlue: UIColor {UIColor(named: "Blue") ?? UIColor.blue}
+}
